41589df43219bc007f23805c27650097a9892700
Jenny Lopez <jenlostar@gmail.com>
2014-10-02 21:59:43 -0500

    - Actualizando test para verificar la respuesta que contenga solo 10 lugares

--- test/integration/listing_places_test.rb
+++ test/integration/listing_places_test.rb
@@ -4,9 +4,13 @@ class ListingPlacesTest < ActionDispatch::IntegrationTest
   setup {host! 'example.com'}
 
   test 'returns list of all places' do
+    create_list(:place, 11)
     get '/api/v1/places'
     assert_equal 200, response.status
     refute_empty response.body
+
+    places_response = json(response.body)
+    assert_equal 10, places_response.length
   end
 
   test 'return place by id' do
