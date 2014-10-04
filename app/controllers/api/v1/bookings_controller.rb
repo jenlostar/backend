3fb9ecad52f8054d8e1229dc77f98ca00dfd907b
Jenny Lopez <jenlostar@gmail.com>
2014-10-04 15:26:16 -0500

    - Mejorando código del controlador api/v1/bookings_controller

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -2,18 +2,14 @@ class API::V1::BookingsController < Api::V1::BaseController
   def create
     booking = Booking.new(booking_params.tap { |bp| bp.delete(:services) })
 
+    Service.where(id: booking_params[:services]).each do |s|
+      booking.booked_services.build(service_name: s.name)
+    end
+
     if booking.save
-      services = Service.find(booking_params[:services])
-
-      services.each do |s|
-        booking.booked_services.create do |bs|
-          bs.service_name = s.name
-        end
-      end
-
-      render json: booking.to_json(:include => :booked_services), status: :ok
+      render json: booking.to_json(include: :booked_services), status: :ok
     else
-      render json: booking.errors, status: 404
+      render json: {errors: booking.errors }, status: 404
     end
   end
 
