4d28f7a99fbbce6e440e7361c96886c07fca7a50
Jenny Lopez <jenlostar@gmail.com>
2014-10-13 22:05:03 -0500

    - Cambiando orden de los dias, para que sea compatible con Javascript

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -4,13 +4,13 @@
 class Schedule < ActiveRecord::Base
   # Constante que define los dias de la semana en un objecto de tipo Hash
   DAYS = {
+    SUNDAY: 0,
     MONDAY: 1,
     TUESDAY: 2,
     WEDNESDAY: 3,
     THURSDAY: 4,
     FRIDAY: 5,
-    SATURDAY: 6,
-    SUNDAY: 7
+    SATURDAY: 6
   }
 
   # Comportamiento por defecto al realizar consultas, ordernamiento
@@ -21,7 +21,7 @@ class Schedule < ActiveRecord::Base
 
   # Retorna el nombre del día de la semana de un horario, este método es
   # necesario debido a que en  base de datos se almacena únicamente un entero
-  # que representa el día de la semana. 1 => Lunes.
+  # que representa el día de la semana. 0 => Domingo.
   def self.day_name(record)
     Schedule::DAYS.key(record.day_of_week).to_s
   end
