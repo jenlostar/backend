d8185d37b31a7f14b1f97f884d68dcfd632a419b
Jenny Lopez <jenlostar@gmail.com>
2014-11-03 11:31:20 -0500

    Eliminando protect_from_forgery

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,8 +1,6 @@
 module API
   module V1
     class BaseController < ActionController::Base
-      protect_from_forgery with: :null_session
-
       def current_resource_owner
         binding.pry
         User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
