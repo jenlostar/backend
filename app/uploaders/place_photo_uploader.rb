1b81afebef9680044b38be0f7c7fc8a1505c07fd
Jenny Lopez <jenlostar@gmail.com>
2014-11-09 14:52:07 -0500

    Actualizando tamaño de foto

--- app/uploaders/place_photo_uploader.rb
+++ app/uploaders/place_photo_uploader.rb
@@ -5,7 +5,7 @@ class PlacePhotoUploader < CarrierWave::Uploader::Base
 
   storage :postgresql_lo
   process convert:  'jpg'
-  process resize_to_fill: [300, 157]
+  process resize_to_fill: [420, 340]
 
   def extension_white_list
     %w(jpg jpeg gif png)
