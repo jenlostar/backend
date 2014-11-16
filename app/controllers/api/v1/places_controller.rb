56b6a8625259d934f28e7a301e735510f4e9835a
Jenny Lopez <jenlostar@gmail.com>
2014-11-16 12:02:16 -0500

    Listando horarios disponibles desde backend

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -25,8 +25,27 @@ module API
       # Reservas de un lugar
       # GET /api/v1/places/:id/bookings/:on_day
       def bookings
-        date = Date.parse(place_params[:on_day])
-        @bookings = @place.bookings.on_day(date)
+        date = Date.parse(place_params[:on_day]).beginning_of_day
+
+        starts = date.advance(:hours => 7).to_i
+        ends = date.advance(:hours => 19).to_i
+
+        hours = (starts..ends).step(1.hour).map do |time|
+          OpenStruct.new(date: Time.at(time), available: true, booking_id: nil)
+        end
+
+        bookings_on_day = @place.bookings.on_day(date)
+
+        @bookings = hours.map do |time|
+          booking = bookings_on_day.detect {|b| b.date ==  time.date}
+
+          unless booking.nil?
+            time.available = false
+            time.booking_id = booking.id
+          end
+
+          time
+        end
       end
 
       private
--- app/views/api/v1/places/_booking.json.jbuilder
+++ app/views/api/v1/places/_booking.json.jbuilder
@@ -1,3 +1,4 @@
-json.id booking.id
+json.booking_id booking.booking_id
 json.hour booking.date.strftime('%H:%M')
 json.hour_formated booking.date.strftime('%I:%M %p')
+json.available booking.available
